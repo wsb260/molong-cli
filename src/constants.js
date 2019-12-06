d8ed184bc32a167c5d94ff5f8245574ee2c28692
wsb260 <260669142@qq.com>
2019-12-06 17:24:58 +0800

    5

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,10 @@
 // 存放用户的所需要的常量
 const { version } = require('../package.json')
 
+// 存放模板的位置
+const downloadDirectory = `${process.env[process.platform === 'darwin' ? 'HOME' : 'USERPROFILE']}\\.template`
+console.log(downloadDirectory)
 module.exports = {
   version,
+  downloadDirectory,
 }
--- src/create.js
+++ src/create.js
@@ -1,6 +1,15 @@
 const axios = require('axios')
 const ora = require('ora')
+const path = require('path')
 const Inquirer = require('inquirer')
+const { promisify } = require('util')
+let downloadGitReop = require('download-git-repo')
+// 把api转换成promise
+downloadGitReop = promisify(downloadGitReop)
+let ncp = require('ncp')
+
+ncp = promisify(ncp)
+const { downloadDirectory } = require('./constants')
 // 1.获取项目列表
 const fetchRepoList = async () => {
   // const { data } = await axios.get('https://github.com/PanJiaChen/vue-admin-template')
@@ -26,7 +35,19 @@ const waitFnloading = (fn, message) => async (...args) => {
   spinner.succeed()
   return result
 }
-
+// 远程下载模板仓库
+const download = async (repo, tag) => {
+  // 仓库路径 组织/用户名 + 仓库名 + 版本号
+  let api = `vuejs-templates/${repo}`
+  if (tag) {
+    api += `#${tag}`
+  }
+  const dest = `${downloadDirectory}\\${repo}`
+  // 参数1：仓库连接，参数2：要存放的地址，参数3：回调函数
+  await downloadGitReop(api, dest)
+  console.log('下载到：', dest)
+  return dest // 下载的最终目录
+}
 
 module.exports = async (projectName) => {
   // 1.获取项目模板
@@ -44,8 +65,21 @@ module.exports = async (projectName) => {
     choices: repos,
   })
   // 2.获取对应的版本
-  console.log(repo)
-  const tags = await waitFnloading(fetchTagList, 'fetching tags ...')(repo)
-  const tag = tags.map((item) => item.name)
-  console.log(tag)
+  let tags = await waitFnloading(fetchTagList, 'fetching tags ...')(repo)
+  tags = tags.map((item) => item.name)
+  const { tag } = await Inquirer.prompt({
+    name: 'tag',
+    type: 'list',
+    message: 'please choices the Version',
+    choices: tags,
+  })
+  console.log(repo, tag)
+  // 3.把模板放到一个临时位置
+  const result = await waitFnloading(download, 'download...')(repo, tag)
+
+  // 4.拿到下载的目录，直接拷贝当前执行的目录下即可 ncp
+
+  // 复杂的需要渲染，渲染后再拷贝
+  // 把template下的文件拷贝到执行命令的目录下
+  ncp(result, path.resolve(projectName))
 }
